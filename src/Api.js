f81a75152cc30232217736422c93628311f88176
Cato <catohjmz08@gmail.com>
2025-03-21 14:04:32 -0600

    visualAlpha

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,5 @@
 const API_URL = process.env.REACT_APP_API_URL
+const API_TEM = process.env.REACT_APP_API_URL_TEMP
 
 export const sendTokenToBackend = async (token) => {
   try {
@@ -16,3 +17,24 @@ export const sendTokenToBackend = async (token) => {
     console.error("Error enviando el token al backend:", error);
   }
 };
+export const getTemperature = async () => {
+  try {
+    const response = await fetch(`${API_TEM}/sensor/data`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al obtener los datos");
+    }
+
+    const data = await response.json();
+    return data; // Devuelve los datos
+  } catch (error) {
+    console.error("Error en getTemperature:", error);
+    return null;
+  }
+};
+
